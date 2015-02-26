406f578bed2f9d187321014a1b99eaa9f4e8a292
Ryan Mulligan <ryan@ryantm.com>
2015-02-26 06:41:51 -0800

    add ledger emacs customization

--- emacs/.emacs.d/custom-file.el
+++ emacs/.emacs.d/custom-file.el
@@ -64,5 +64,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ledger-font-xact-highlight-face ((t nil)))
  '(whitespace-newline ((t (:foreground "gray35" :weight ultra-light))))
  '(whitespace-space ((t (:foreground "grey35" :weight ultra-light)))))
