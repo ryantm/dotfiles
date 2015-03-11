ed148c5c322ea6709c8fa73a6d60bf91bb646b14
Ryan Mulligan <ryan@ryantm.com>
2015-03-10 20:45:35 -0700

    add default.nix for building xmonad

---
+++ xmonad/.xmonad/default.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+in rec {
+  myXmonad = stdenv.mkDerivation rec {
+    name = "myXmonad";
+    buildInputs = [
+      stdenv
+      pkgs.haskellPackages.ghc
+      pkgs.haskellPackages.xmonad
+      pkgs.haskellPackages.xmobar
+      pkgs.haskellPackages.xmonadContrib ];
+    src = "xmonad.hs";
+    builder =
+    ''
+      ghc xmonad.hs
+    '';
+  };
+}
