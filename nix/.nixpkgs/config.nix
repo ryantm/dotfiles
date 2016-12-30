2638304ba6dd30b65562b3e8c3627e5a1e560708
Ryan Mulligan <ryan@ryantm.com>
2016-12-29 17:15:30 -0800

    add installation instructions

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,5 +1,8 @@
 {
   allowUnfree = true;
+
+  # Install with
+  #   nix-env -iA nixpkgs.all
   packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
     all = wit pkgs; buildEnv {  # pkgs is your overriden set of packages itself
       name = "all";
