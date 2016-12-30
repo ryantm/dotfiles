81d4e6ae0c4d4e5955f40a86b49ef43ee017fcb8
Ryan Mulligan <ryan@ryantm.com>
2016-12-29 17:14:34 -0800

    add user-level package declarations

---
+++ nix/.nixpkgs/config.nix
@@ -0,0 +1,26 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
+    all = wit pkgs; buildEnv {  # pkgs is your overriden set of packages itself
+      name = "all";
+      paths = [
+        firefox
+        ledger
+        thunderbird
+        remmina
+        vlc
+        scrot
+        gimp
+        calibre
+        libreoffice
+        qbittorrent
+        fbreader
+        usbutils
+        zsnes
+        ruby
+        python
+        evince
+      ];
+    };
+  };
+}
