a251f4706ff8cec20b2731b9920019bae49c5ccc
Ryan Mulligan <ryan@ryantm.com>
2017-04-07 01:13:37 -0700

    update installation instructions for nixpkgs

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -6,8 +6,12 @@
     enableWideVine = true;
   };
 
-  # Install with
+  # On non-nixos install with
   #   nix-env -iA nixpkgs.all
+
+  # On nixos install with
+  #   nix-env -iA pkgs.all
+
   packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
     all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
       name = "all";
