f1cbe54b0408daed8e65b46e99abad227a99a0c5
Ryan Mulligan <ryan@ryantm.com>
2020-01-01 21:33:31 -0800

    bump nixpkgs pin

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ln -fs "$DIR/home.nix" "$nixpkgsConfig/home.nix"
 
 #/home/ryantm/p/nixpkgs
 
-NIX_PATH=nixpkgs=https://releases.nixos.org/nixos/19.09/nixos-19.09.1320.4ad6f1404a8/nixexprs.tar.xz 
+NIX_PATH=nixpkgs=https://releases.nixos.org/nixos/unstable/nixos-20.03pre206632.b0bbacb5213/nixexprs.tar.xz
 HM_PATH=${HM_PATH:-https://github.com/rycee/home-manager/archive/master.tar.gz}
 
 nix-shell $HM_PATH -A install
