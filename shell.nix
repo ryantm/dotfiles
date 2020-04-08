4514ce7d0e3ccb34bfab106af17b5d95b0b9b2c7
Ryan Mulligan <ryan@ryantm.com>
2020-04-07 21:37:37 -0700

    we don't need installing

--- shell.nix
+++ shell.nix
@@ -10,21 +10,14 @@ in pkgs.mkShell rec {
   buildInputs = with pkgs; [
     nix
     niv
+    sources.home-manager
   ];
 
   shellHook = ''
     export NIX_PATH="nixpkgs=${sources."nixos-20.03"}"
-    HM_PATH=$${HM_PATH:-https://github.com/rycee/home-manager/archive/master.tar.gz}
-    DIR="$( cd "$( dirname "$${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
     function switch () {
       home-manager switch
-    }
-
-    function install () {
-      mkdir -p $nixpkgsConfig
-      ln -fs "$DIR/home.nix" "$nixpkgsConfig/home.nix"
-      nix-shell $HM_PATH -A install
     }
   '';
 
