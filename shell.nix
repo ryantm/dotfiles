372e648767d14ae0d8d26ec84c48fda54629c2e3
Ryan Mulligan <ryan@ryantm.com>
2020-06-28 20:32:33 -0700

    use nixpkgs-unstable, update shell.nix to match home-manager template

--- shell.nix
+++ shell.nix
@@ -1,25 +1,21 @@
 let
 
   sources = import ./nix/sources.nix;
-  pkgs = import sources."nixos-20.03" {};
+  nixpkgs = sources."nixpkgs-unstable";
+  pkgs = import nixpkgs {};
 
 in pkgs.mkShell rec {
 
-  name = "dotfiles-shell";
+  name = "home-manager-shell";
 
   buildInputs = with pkgs; [
-    nix
     niv
-    sources.home-manager
+    (import sources.home-manager {inherit pkgs;}).home-manager
   ];
 
   shellHook = ''
-    export NIX_PATH="nixpkgs=${sources."nixos-20.03"}:home-manager=${sources."home-manager"}"
+    export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
     export HOME_MANAGER_CONFIG="./home.nix"
-
-    function switch () {
-      home-manager switch
-    }
   '';
 
 }
